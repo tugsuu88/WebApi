790fad977ac6fae5170c90ef2d89054d059cc8f2
tugsuu88 <hero_lion74@yahoo.com>
2018-01-15 22:32:20 +0800

    Video

---
+++ WebApi/WebApi/Controllers/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+using WebApi.Streams;
+
+namespace WebApi.Controllers
+{
+    public class CameraController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+
+
+            return new string[] { "Tugsuu", "Lion" };
+        }
+
+        [HttpGet]
+        public HttpResponseMessage FromVideo(string videoName)
+        {
+            var video = new VideoStream(videoName);
+            Func<Stream, HttpContent, TransportContext, Task> func = video.WriteToStream;
+            var response = Request.CreateResponse();
+            response.Content = new PushStreamContent(func, new MediaTypeHeaderValue("video/mp4"));
+
+            return response;
+        }
+    }
+}
---
+++ WebApi/WebApi/Streams/VideoStream.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApi.Streams
+{
+    internal class VideoStream
+    {
+        private readonly string videoName;
+
+        public VideoStream(string videoName)
+        {
+            this.videoName = videoName;
+        }
+
+        public async Task WriteToStream(Stream outputStream, HttpContent content, TransportContext context)
+        {
+            string videoFileName = "TestData\\" + videoName + ".mp4";
+            try
+            {
+                var buffer = new byte[65536];
+
+                using (var video = File.Open(@"D:\Projects\RaspberryDashboard\API\WebApi\WebApi\TestData\" + videoName + @".mp4", FileMode.Open, FileAccess.Read))
+                {
+                    var length = (int)video.Length;
+                    var bytesRead = 1;
+
+                    while (length > 0 && bytesRead > 0)
+                    {
+                        bytesRead = video.Read(buffer, 0, Math.Min(length, buffer.Length));
+                        await outputStream.WriteAsync(buffer, 0, bytesRead);
+                        length -= bytesRead;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            finally
+            {
+                outputStream.Close();
+            }
+        }
+    }
+}
